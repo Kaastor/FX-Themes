3912b896e0f9c81450a7b3d4334f1d6016b329d5
Kaastor <czuba.przemyslaw@gmail.com>
2016-10-22 15:52:49 +0200

    Initial commit

---
+++ src/main/java/com/gluonapplication/JavaFX.java
@@ -0,0 +1,45 @@
+package com.gluonapplication;
+
+import com.aquafx_project.AquaFx;
+import com.guigarage.flatterfx.FlatterFX;
+import org.aerofx.AeroFX;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class JavaFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("GUI L&F");
+        Button aquaFxButton = new Button("AquaFx");
+        Button aeroFxButton = new Button("AeroFx");
+        Button flatterButton = new Button("Flatter");
+        Button modenaButton = new Button("Default - Modena");
+        Button caspianButton = new Button("Default - Caspian");
+
+        ListView<String> listView = new ListView<>();
+        listView.getItems().addAll("AquaFx", "AeroFx", "AquaFx");
+
+        aquaFxButton.setOnAction(e -> AquaFx.style());
+        aeroFxButton.setOnAction(e -> AeroFX.style());
+        flatterButton.setOnAction(e -> FlatterFX.style());
+        modenaButton.setOnAction(e -> setUserAgentStylesheet(STYLESHEET_MODENA));
+        caspianButton.setOnAction(e -> setUserAgentStylesheet(STYLESHEET_CASPIAN));
+
+        VBox vBox= new VBox();
+
+        vBox.getChildren().addAll(aquaFxButton, aeroFxButton, flatterButton,
+                modenaButton, caspianButton, listView);
+
+        Scene scene = new Scene(vBox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
